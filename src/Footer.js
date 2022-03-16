2348aa6c9e98132a586497c717d1dc536b276ee2
Sam Quinn <samquinn20@gmail.com>
2022-03-16 21:07:24 +0000

    add player controls

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,53 @@
+import Grid from "@material-ui/core/Grid";
+import Slider from "@material-ui/core/Slider";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import SkipPrevious from "@material-ui/icons/SkipPrevious";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
 import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer__left">
+        <img
+          className="footer__albumLogo"
+          src="https://upload.wikimedia.org/wikipedia/en/2/2e/Usher-yeah.jpg"
+          alt=""
+        />
+        <div className="footer__songInfo">
+          <h4>Yeah!</h4>
+          <p>Usher</p>
+        </div>
+      </div>
+
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPrevious className="footer__icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
+      </div>
+
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
