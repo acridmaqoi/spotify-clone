f383b031e3baa16d8d18fbc1cfbb275dd7f6d404
Sam Quinn <samquinn20@gmail.com>
2022-03-17 16:55:43 +0000

    display songs

--- src/App.js
+++ src/App.js
@@ -36,6 +36,13 @@ function App() {
           playlists: playlists,
         });
       });
+
+      spotify.getPlaylist("37i9dQZEVXcH5PbvNyXSPr").then((response) => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        });
+      });
     }
   }, []);
 
--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,7 @@ export const initalState = {
   playlists: [],
   playing: false,
   item: null,
+  discover_weekly: null,
   //token:
   //  "BQBx3IW58Zdj9ZCZ3B4SK6AAeMstfz1snm6VByBWtLvvJWQ-OemP5m6HuHV1cup0ce-lz0xi8JhLRPPnFnvbiER18f7zveSE6k-WN30i916XTAmlB-Q-QxpABQeY7IP2J4WlQ2v7GJFrZgjNlRQCDpMzwfyj2I3d",
 };
@@ -27,6 +28,12 @@ const reducer = (state, action) => {
       return {
         ...state,
         playlists: action.playlists,
+      };
+
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
       };
 
     default:
