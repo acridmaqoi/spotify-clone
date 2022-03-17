dd708c85f959ed1d0369ccdea45c130316616b0c
Sam Quinn <samquinn20@gmail.com>
2022-03-17 17:39:36 +0000

    add onclick handler to play song from list

--- src/reducer.js
+++ src/reducer.js
@@ -4,6 +4,7 @@ export const initalState = {
   playing: false,
   item: null,
   discover_weekly: null,
+  playing: false,
   //token:
   //  "BQBx3IW58Zdj9ZCZ3B4SK6AAeMstfz1snm6VByBWtLvvJWQ-OemP5m6HuHV1cup0ce-lz0xi8JhLRPPnFnvbiER18f7zveSE6k-WN30i916XTAmlB-Q-QxpABQeY7IP2J4WlQ2v7GJFrZgjNlRQCDpMzwfyj2I3d",
 };
@@ -16,6 +17,18 @@ const reducer = (state, action) => {
       return {
         ...state,
         user: action.user,
+      };
+
+    case "SET_PLAYING":
+      return {
+        ...state,
+        playing: action.playing,
+      };
+
+    case "SET_ITEM":
+      return {
+        ...state,
+        item: action.item,
       };
 
     case "SET_TOKEN":
